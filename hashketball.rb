105bb9c38e9d7577cb0d0b8d79deb4a1af458db9
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-18 18:36:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -84,6 +84,15 @@ def game_hash
         :steals => 4,
         :blocks => 5,
         :slam_dunks => 5},
+        {:player_name => "DeSagna Diop",
+        :number => 2, 
+        :shoe => 14, 
+        :points => 24,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5,
+        :slam_dunks => 5},
         ]
     }
     
