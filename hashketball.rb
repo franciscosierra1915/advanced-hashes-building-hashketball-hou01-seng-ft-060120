9dd9cae7c9fde6927a33afce7b909c78f1ae4fb3
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-18 17:58:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,19 @@
 # Write your code here!
+def game_hash
+  game_big_hash = {
+    :home => {
+      :team_name => "Astros",
+      :colors, 
+      :players
+    },
+    :away => {}
+    
+    
+    
+    
+    
+    
+    
+  }
+  
+end
